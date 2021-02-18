9a7250a91e8e3eb2d935cf8d67f24dd8db8229e1
sousquared <sou.yoshihara.72e@gmail.com>
2021-02-18 16:14:11 +0900

    Add imsave function.

--- imagenet16/src/utils/image.py
+++ imagenet16/src/utils/image.py
@@ -1,7 +1,7 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from matplotlib.image import imsave
 
 
 def imshow(
@@ -9,24 +9,45 @@ def imshow(
 ):
     if unnormalize:
         img = img / 4 + 0.5
+
     if type(img) == torch.Tensor:
         img = img.numpy().transpose(1, 2, 0)
+
     if dpi:
         plt.figure(dpi=dpi)
+
     plt.imshow(img)
+
     if not scale:
         plt.xticks([])  # if you want to remove scale axes
         plt.yticks([])
+
     if title:
         plt.title(title)
+
     if filename:
         if save_raw:
             # clipping
             img = np.where(img > 1, 1, img)
             img = np.where(img < 0, 0, img)
 
-            imsave(filename, img)  # save the raw image
+            matplotlib.image.imsave(filename, img)  # save the raw image
 
         else:
             plt.savefig(filename)
+
     plt.show()
+
+
+def imsave(img, filename, unnormalize=False):
+    if unnormalize:
+        img = img / 4 + 0.5
+
+    if type(img) == torch.Tensor:
+        img = img.numpy().transpose(1, 2, 0)
+
+    # clipping
+    img = np.where(img > 1, 1, img)
+    img = np.where(img < 0, 0, img)
+
+    matplotlib.image.imsave(filename, img)  # save the raw image
