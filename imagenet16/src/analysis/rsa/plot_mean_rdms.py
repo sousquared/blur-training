16cf373cd978f2b36abc1191423830ee1632e548
sousquared <sou.yoshihara.72e@gmail.com>
2021-02-22 18:32:05 +0900

    Update band-pass RDMs.

--- imagenet16/src/analysis/rsa/plot_mean_rdms.py
+++ imagenet16/src/analysis/rsa/plot_mean_rdms.py
@@ -86,10 +86,10 @@ def plot_rdms(in_dir, model_name, epoch, out_dir):
 if __name__ == "__main__":
     arch = "alexnet"
     epoch = 60
+
     analysis_name = f"{arch}_bandpass"
     in_dir = f"./results/{analysis_name}"
     out_dir = f"./plots/{analysis_name}"
-
     assert os.path.exists(in_dir), f"{in_dir} does not exist."
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
@@ -98,53 +98,39 @@ if __name__ == "__main__":
     # models to plot
     modes = [
         "normal",
-        "all",
-        "mix",
-        "random-mix",
-        "single-step",
-        "fixed-single-step",
-        "reversed-single-step",
-        "multi-steps",
+        # "all",
+        # "mix",
+        # "random-mix",
+        # "single-step",
+        # "fixed-single-step",
+        # "reversed-single-step",
+        # "multi-steps",
     ]
 
-    # sigmas to compare
+    # sigmas to plot
     sigmas_mix = [s for s in range(1, 6)] + [10]
     sigmas_random_mix = ["00-05", "00-10"]
 
-    # plot and save
+    # make model name list
+    model_names = []
     for mode in modes:
         if mode in ("normal", "multi-steps"):
-            model_name = f"{arch}_{mode}"
-            plot_rdms(
-                in_dir=in_dir,
-                model_name=model_name,
-                epoch=epoch,
-                out_dir=out_dir,
-            )
+            model_names += [f"{arch}_{mode}"]
         elif mode == "random-mix":
             for min_max in sigmas_random_mix:
-                model_name = "{}_{}_s{}".format(arch, mode, min_max)
-                plot_rdms(
-                    in_dir=in_dir,
-                    model_name=model_name,
-                    epoch=epoch,
-                    out_dir=out_dir,
-                )
+                model_names += [f"{arch}_{mode}_s{min_max}"]
         elif mode == "mix":
             for sigma in sigmas_mix:
-                model_name = f"{arch}_{mode}_s{sigma:02d}"
-                plot_rdms(
-                    in_dir=in_dir,
-                    model_name=model_name,
-                    epoch=epoch,
-                    out_dir=out_dir,
-                )
+                model_names += [f"{arch}_{mode}_s{sigma:02d}"]
         else:
             for s in range(4):
-                model_name = f"{arch}_{mode}_s{s + 1:02d}"
-                plot_rdms(
-                    in_dir=in_dir,
-                    model_name=model_name,
-                    epoch=epoch,
-                    out_dir=out_dir,
-                )
+                model_names += [f"{arch}_{mode}_s{s + 1:02d}"]
+
+    # plot and save
+    for model_name in model_names:
+        plot_rdms(
+            in_dir=in_dir,
+            model_name=model_name,
+            epoch=epoch,
+            out_dir=out_dir,
+        )
