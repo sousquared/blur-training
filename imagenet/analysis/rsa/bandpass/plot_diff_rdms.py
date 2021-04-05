4a86a830840ac5b0c5b574a036a84cd399b46082
sousquared <sou.yoshihara.72e@gmail.com>
2021-04-05 15:55:01 +0900

    Add "diff of RDMs."

--- imagenet/analysis/rsa/bandpass/plot_diff_rdms.py
+++ imagenet/analysis/rsa/bandpass/plot_diff_rdms.py
@@ -24,34 +24,37 @@ if __name__ == "__main__":
     model_name = f"{arch}_{mode}"
     rdms_normal = load_rdms(in_dir=in_dir, model_name=model_name, epoch=epoch)
 
-    mode = "mix_s04"
-    model_name = f"{arch}_{mode}"
-    rdms_mix04 = load_rdms(in_dir=in_dir, model_name=model_name, epoch=epoch)
+    # models to compare
+    blur_models = ["mix_s04", "all_s04"]
 
-    diff_rdms = {}
-    for layer in alexnet_layers:
-        diff_rdms[layer] = rdms_mix04[layer] - rdms_normal[layer]
+    for blur_model in blur_models:
+        model_name = f"{arch}_{blur_model}"
+        rdms_blur = load_rdms(in_dir=in_dir, model_name=model_name, epoch=epoch)
 
-    # get analysis parameters.
-    num_images = rdms_normal["num_images"]
-    num_filters = rdms_normal["num_filters"]
+        diff_rdms = {}
+        for layer in alexnet_layers:
+            diff_rdms[layer] = rdms_blur[layer] - rdms_normal[layer]
 
-    # (optional) set title
-    title = f"1000-class-{arch}, mix_s04 - normal, epoch={epoch}"
+        # get analysis parameters.
+        num_images = rdms_normal["num_images"]
+        num_filters = rdms_normal["num_filters"]
 
-    # set filename
-    filename = "1000-class_diff_normal_mix04_e{}_f{}_n{}.png".format(
-        epoch, num_filters, num_images
-    )  # add "target_id" if you need it.
-    out_file = os.path.join(out_dir, filename)
+        # (optional) set title
+        title = f"16-class-{arch}, {blur_model} - normal, epoch={epoch}"
 
-    # plot_rdms(rdms=diff_rdms, out_file=out_file, plot_show=True)
-    plot_bandpass_rdms(
-        rdms=diff_rdms,
-        num_filters=num_filters,
-        vmin=-0.5,
-        vmax=0.5,
-        title=title,
-        out_file=out_file,
-        show_plot=False,
-    )
+        # set filename
+        filename = "16-class_diff_normal_{}_e{}_f{}_n{}.png".format(
+            blur_model, epoch, num_filters, num_images
+        )  # add "target_id" if you need it.
+        out_file = os.path.join(out_dir, filename)
+
+        # plot_rdms(rdms=diff_rdms, out_file=out_file, plot_show=True)
+        plot_bandpass_rdms(
+            rdms=diff_rdms,
+            num_filters=num_filters,
+            vmin=-0.5,
+            vmax=0.5,
+            title=title,
+            out_file=out_file,
+            show_plot=False,
+        )
