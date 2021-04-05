7587a57ad1092c6c12d4a150c163c33e91d1db7b
sousquared <sou.yoshihara.72e@gmail.com>
2021-04-05 18:26:39 +0900

    Format files.

--- src/dataset/imagenet16.py
+++ src/dataset/imagenet16.py
@@ -3,7 +3,6 @@ import pathlib
 
 import torch
 import torchvision.transforms as transforms
-
 from robustness import datasets
 from robustness.tools.imagenet_helpers import common_superclass_wnid, ImageNetHierarchy
 
