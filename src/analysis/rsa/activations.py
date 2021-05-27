f7622b4259a05a7aef6ac3295435e6712fb7e085
sousquared <sou.yoshihara.72e@gmail.com>
2021-05-27 18:22:02 +0900

    Update.

--- src/analysis/rsa/activations.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pickle
-
-
-def load_activations(in_dir, file_name):
-    file_path = os.path.join(in_dir, file_name)
-    with open(file_path, "rb") as f:
-        return pickle.load(f)
